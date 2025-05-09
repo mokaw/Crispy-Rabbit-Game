d055476a62ecd03f6aa40b8f813cb48143f359c0
mokaw <63728712+mokaw@users.noreply.github.com>
2025-05-09 13:56:15 +0200

    adding parallax background

---
+++ Assets/Scripts/parallaxBackground.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxBackground : MonoBehaviour
+{
+
+    private float startPos;
+    private float length;
+    private GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = this.GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+        if( temp > startPos + length)
+        {
+            startPos += length;
+        }
+    }
+}
